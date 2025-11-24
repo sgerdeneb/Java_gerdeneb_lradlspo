7d82d8203911554c583e4e23efe34690ddabddce
Ganerdene Erdenebayar <gerdeneb@htl-steyr.ac.at>
2025-11-24 13:50:58 +0100

    add gerdeneb.java + ganaa object in ProjectMain

---
+++ src/java/Gerdeneb.java
@@ -0,0 +1,5 @@
+class Gerdeneb implements TellYourName {
+    public void tell(){
+        System.out.println("Ganerdene Erdenebayar");
+    }
+}
--- src/java/ProjectMain.java
+++ src/java/ProjectMain.java
@@ -1,5 +1,7 @@
 class ProjectMain {
     public static void main(String[] args) {
+        Gerdeneb ganaa = new Gerdeneb;
 
+        ganaa.tell();
     }
 }
